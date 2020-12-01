2c2f262ff1e1dd089413ecc8ffda68b17ddd68bf
奉眠 <33065020+Astral-cyber@users.noreply.github.com>
2020-12-01 16:59:12 +0800

    Add files via upload
    基本框架思路

--- Code/main.c
+++ Code/main.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-
-int fibonaci(int i)
-{
-    if(i == 0)
-    {
-        return 0;
+#define N 5
+int max(int a,int b);
+int main() {
+    int x,y;
+    int r[N][N]={0};//初始化屋顶数组
+    FILE *down=fopen("/Users/astral/CLionProjects/project3/ice/hail.txt","r");//打开文件（使用绝对路径）
+    while (!feof(down)) {//获取冰雹落点
+        fscanf(down,"%d %d",&x,&y);
+        if (x>N||y>N)//防止用户输入溢出
+            break;
+        x=x-1;//行数转化为数组编号
+        y=y-1;//列数转化为数组编号
+        //中心位置形变
+        r[x][y]+=4;
+        //外圈形变循环
+        //最外圈形变循环
+        //调试部分
+        for (int i = 0; i < N; ++i) {//输出冰雹后屋顶
+            for (int j = 0; j < N; ++j) {
+                printf("%d\t",r[i][j]);
+            }
+            printf("\n");
+        }
+        printf("--------------------------\n");//分界线
+        //调试部分输出每一颗冰雹对数组的影响
     }
-    if(i == 1)
-    {
-        return 1;
+    fclose(down);//关闭文件
+    printf("屋顶形变情况为：\n");
+    for (int i = 0; i < N; ++i) {//输出冰雹后屋顶
+        for (int j = 0; j < N; ++j) {
+            printf("%d\t",r[i][j]);
+        }
+        printf("\n");
     }
-    return fibonaci(i-1) + fibonaci(i-2);
-}
-
-int  main()
-{
-    int i;
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\t\n", fibonaci(i));
-    }
+    printf("形变最大的位置是：");
     return 0;
+}
+int max(int a,int b){
+
 }
