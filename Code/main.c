1dffd133c5bb24f3ad9b21a1803324580813dbd6
奉眠 <33065020+Astral-cyber@users.noreply.github.com>
2020-12-03 14:14:17 +0800

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char name[20];
+    char name1[20],name2[20];
     FILE *enemies = fopen("../Name-list/Enemies.txt", "r");//打开文件
-    
-    while (!feof(enemies)){
-        fscanf(enemies,"%s",name);
-        printf("%s\n",name);
+    while (!feof(enemies)){//获取每一行的两个名字，分别储存为name1和name2
+        fscanf(enemies,"%s %s",name1,name2);
+        printf("%s %s\n",name1,name2);
     }
     return 0;
 }
